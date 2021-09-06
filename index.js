b38d986a56c46787ba39cb1ff979fdb717bb21b2
LuisP <luisjavierpolancomorel@gmail.com>
2021-09-05 21:04:30 -0400

    type module deleted

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-
-
-const toggle = document.querySelector('input');
-const price = document.querySelectorAll('#cards div .price');
-
-const pricesArr = Array.from(price);
-
-const anualPricing = (pricesArr = []) => {
-
-    return pricesArr.map(price => {
-        const text = price.innerText.split('');
-        text.splice(3, 0, '9');
-        price.innerText = text.join('')
-        return text; 
-    });
-
-}
-
-const monthlyPricing = (pricesArr = []) => {
-
-    return pricesArr.map(price => {
-        const text = price.innerText.split('');
-        if (text.length >= 7) {
-            text.splice(3, 1);
+(() => {
+    
+    const toggle = document.querySelector('input');
+    const price = document.querySelectorAll('#cards div .price');
+    
+    const pricesArr = Array.from(price);
+    
+    const anualPricing = (pricesArr = []) => {
+    
+        return pricesArr.map(price => {
+            const text = price.innerText.split('');
+            text.splice(3, 0, '9');
             price.innerText = text.join('')
             return text; 
-        } else {
-            return text.join('');
-        }
+        });
+    
+    }
+    
+    const monthlyPricing = (pricesArr = []) => {
+    
+        return pricesArr.map(price => {
+            const text = price.innerText.split('');
+            if (text.length >= 7) {
+                text.splice(3, 1);
+                price.innerText = text.join('')
+                return text; 
+            } else {
+                return text.join('');
+            }
+            
+        });
+    
+    }
+    
+    
+    
+    
+    toggle.addEventListener('change', () => {
         
+        if (!toggle.checked) {
+            anualPricing(pricesArr);
+        } else {
+            return monthlyPricing(pricesArr);
+        }
+    
     });
 
-}
-
-
-
-
-toggle.addEventListener('change', () => {
-    
-    if (!toggle.checked) {
-        anualPricing(pricesArr);
-    } else {
-        return monthlyPricing(pricesArr);
-    }
-
-})
+})();
